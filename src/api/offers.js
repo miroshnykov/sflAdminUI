b532b88fe292198951c01cd48000176862a4731a
miroshnykov <dmytro.miroshnykov@adsurge.com>
2021-01-20 15:39:05 -0500

    added dateUpdated

--- src/api/offers.js
+++ src/api/offers.js
@@ -16,6 +16,7 @@ const getOffers = async () => {
                                 payIn
                                 payOut
                                 dateAdded
+                                dateUpdated
                           }
                     }
             `,
